b6c7ff33e0886635275435d605c47b0c761cf7cd
jazzioat <159167142+jazzioat@users.noreply.github.com>
2024-06-20 22:04:16 -0700

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // JavaScript is currently not required but this ensures that any future scripts
+    // can safely run after the DOM is fully loaded.
+});
